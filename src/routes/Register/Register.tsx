80ed186a77d7a4736d966b5f741ca86b6132927a
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-14 22:08:01 +0800

    refactor: remove redundancy in register submit form

--- src/routes/Register/Register.tsx
+++ src/routes/Register/Register.tsx
@@ -2,30 +2,28 @@ import type { FormEvent } from "react";
 
 const Register = () => {
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const formData = new FormData(e.currentTarget);
-    const data = {
-      username: formData.get("username")?.toString(),
-      password: formData.get("password")?.toString(),
-    };
-    const response = await fetch("http://localhost:3000/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(data),
-    });
-    const result = await response.json();
-    console.log(result);
+    try {
+      e.preventDefault();
+      const formData = new FormData(e.currentTarget);
+      const data = {
+        username: formData.get("username"),
+        password: formData.get("password"),
+      };
+      const response = await fetch("http://localhost:3000/register", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const result = await response.json();
+      console.log(result);
+    } catch (err) {
+      console.log("Failed to register user.", err);
+    }
   };
   return (
     <div>
       <h1>Register</h1>
-      <form
-        action="http://localhost:3000/register"
-        method="post"
-        onSubmit={handleSubmit}
-      >
+      <form onSubmit={handleSubmit}>
         <label htmlFor="username">Username</label>
         <input name="username" id="username" type="text" required />
         <label htmlFor="password">Password</label>
