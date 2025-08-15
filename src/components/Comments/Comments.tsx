e28627d8c6c2baa630fb4995bc9418bb09a6fd57
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-15 15:07:27 +0800

    feat: add new comments with auth

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState, type JSX } from "react";
+import { useEffect, useState, type FormEvent, type JSX } from "react";
 import type { CommentType } from "../../types";
 
 type CommentsProps = {
@@ -20,6 +20,32 @@ const Comments = ({ postId }: CommentsProps): JSX.Element => {
     fetchComments();
   }, [postId]);
 
+  const handlePostComment = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const data = {
+      comment: formData.get("comment") || "",
+    };
+    const token = localStorage.getItem("token") || "";
+    console.log(token);
+    const response = await fetch(
+      `http://localhost:3000/posts/${postId}/comments`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    const result = await response.json();
+    setComments((prev) =>
+      prev ? [result.comment, ...prev] : [result.comment]
+    );
+    console.log(result);
+  };
+
   if (!comments) {
     return <div>Error</div>;
   }
@@ -27,6 +53,11 @@ const Comments = ({ postId }: CommentsProps): JSX.Element => {
   return (
     <div>
       <h1>Comments Section:</h1>
+      <form onSubmit={handlePostComment}>
+        <label htmlFor="comment">Comment</label>
+        <input type="text" id="comment" name="comment" />
+        <button type="submit">Post</button>
+      </form>
       {comments.map((comment) => (
         <span key={comment.id}>
           {comment.comment} {new Date(comment.createdAt).toLocaleString()}
