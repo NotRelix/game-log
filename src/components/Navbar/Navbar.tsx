a707d1266a3402c1946bdf9c4d376730ca146adb
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-15 22:41:07 +0800

    feat: add profile menu with toggle open/close

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import styles from "./Navbar.module.css";
 import { DarkModeContext } from "../../context/DarkModeContext";
-import { Moon, Sun } from "lucide-react";
+import { CircleUserRound, Moon, Sun } from "lucide-react";
 import { Link } from "react-router";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
   const context = useContext(DarkModeContext);
   if (!context)
     throw new Error("Navbar must be used inside a DarkModeProvider");
@@ -12,7 +13,9 @@ const Navbar = () => {
   return (
     <nav className={`${styles.navbar} ${darkMode && styles.dark}`}>
       <div>
-        <Link className={styles.logo} to={"/"}>Game Log</Link>
+        <Link className={styles.logo} to={"/"}>
+          Game Log
+        </Link>
       </div>
       <div className={styles.menuContainer}>
         {darkMode ? (
@@ -20,6 +23,27 @@ const Navbar = () => {
         ) : (
           <Moon className={styles.toggleTheme} onClick={toggleDarkMode} />
         )}
+        <div className={styles.profileContainer}>
+          <CircleUserRound
+            onClick={() => setMenuOpen(!menuOpen)}
+            className={styles.profileIcon}
+          />
+          <div
+            className={`${styles.menuDropDown} ${menuOpen ? styles.show : ""} ${
+              darkMode ? styles.dark : ""
+            }`}
+          >
+            <span>User</span>
+            <span>User</span>
+            <span>User</span>
+          </div>
+          <div
+            onClick={() => setMenuOpen(false)}
+            className={`${styles.backdrop} ${
+              menuOpen ? styles.showBackdrop : ""
+            }`}
+          ></div>
+        </div>
       </div>
     </nav>
   );
