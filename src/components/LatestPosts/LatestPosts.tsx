c9a4784c4bba4f6efd52814ed3b8ade08c3f1480
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-22 22:09:48 +0800

    feat: add loading to latest posts

--- src/components/LatestPosts/LatestPosts.tsx
+++ src/components/LatestPosts/LatestPosts.tsx
@@ -7,6 +7,17 @@ interface LatestPostsType {
   loading: boolean;
 }
 
+const emptyPost: PostType = {
+  id: -1,
+  title: "",
+  body: "",
+  published: false,
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  authorId: -1,
+  headerImgPath: "",
+};
+
 const LatestPosts = ({ posts, loading }: LatestPostsType) => {
   return (
     <section className={styles.latestPostsContainer}>
@@ -15,9 +26,13 @@ const LatestPosts = ({ posts, loading }: LatestPostsType) => {
         <p>Check out the most recent posts from your favorite bloggers.</p>
       </div>
       <div className={styles.postsContainer}>
-        {posts.map((post) => (
-          <MiniPost key={post.id} post={post} loading={loading} />
-        ))}
+        {posts.length === 0
+          ? Array.from({ length: 6 }).map((_, i) => (
+              <MiniPost key={i} post={emptyPost} loading={loading} />
+            ))
+          : posts.map((post) => (
+              <MiniPost key={post.id} post={post} loading={loading} />
+            ))}
       </div>
     </section>
   );
