171c7d33c92e134f1df6833798271b59a09fc71f
Reece Lim <111989096+NotRelix@users.noreply.github.com>
2025-08-25 14:46:26 +0800

    feat: add reply button to comments

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,22 +1,54 @@
-import type { CommentType } from "../../types"
+import { useContext } from "react";
+import type { CommentType } from "../../types";
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
-import styles from "./Comment.module.css"
+import styles from "./Comment.module.css";
+import { DarkModeContext } from "../../context/DarkModeContext";
+import { PopupContext } from "../../context/PopupContext";
+import { useAuth } from "../../hooks/useAuth";
 
 interface CommentProps {
   comment: CommentType;
 }
 
 const Comment = ({ comment }: CommentProps) => {
+  const { isAuthenticated } = useAuth();
+  const darkModeContext = useContext(DarkModeContext);
+  const popupContext = useContext(PopupContext);
+  if (!darkModeContext)
+    throw new Error("Comment must be used inside a DarkModeProvider");
+  if (!popupContext) {
+    throw new Error("Comment must be used inside a PopupProvider");
+  }
+  const { darkMode } = darkModeContext;
+  const { setLoginPopupOpen, setIsLoginPopupVisible } = popupContext;
+
+  const handleReplyClick = () => {
+    if (!isAuthenticated) {
+      setLoginPopupOpen(true);
+      requestAnimationFrame(() => {
+        setIsLoginPopupVisible(true);
+      });
+      return;
+    }
+  };
+
   console.log(comment);
   return (
-    <div className={styles.commentContainer}>
+    <div
+      className={`${styles.commentContainer} ${darkMode ? styles.dark : ""}`}
+    >
       <ProfilePicture username={comment.author.username} />
       <div className={styles.commentRightContainer}>
         <span>@{comment.author.username}</span>
         <span>{comment.comment}</span>
+        <div>
+          <button className={styles.reply} onClick={handleReplyClick}>
+            Reply
+          </button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Comment
+export default Comment;
